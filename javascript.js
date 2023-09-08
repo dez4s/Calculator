3ac88acf436831d58f6c9eb66bbadd5b89d55666
Gabriel Panaitescu <124690179+dez4s@users.noreply.github.com>
2023-09-08 13:52:22 +0300

    Update the UI design

--- javascript.js
+++ javascript.js
@@ -2,7 +2,7 @@ function Calculator() {
     this['+'] = (a, b) => a + b;
     this['-'] = (a, b) => a - b;
     this['*'] = (a, b) => a * b;
-    this['/'] = (a, b) => a / b;
+    this['÷'] = (a, b) => a / b;
 
     this.calculate = (a, b, op) => {
         return this[op](a, b);
@@ -18,16 +18,17 @@ let result;
 
 function getNumber() {
     if (!op) {
-        if (!a) {
+        if (!a) { // if `a` will be equal to 0 (number type not string), the equality test will return true and it will overwrite the 0 value assigned to `a`. Alternative 'if (a === null)' =>  The equality test returns true only if `a` is null and only then it will assign a new value to `a`, not if `a` is equal to 0. Both methods work and it depends on the type of behavior is wanted for the calculator to have.
             result = null;
             a = this.getAttribute('data-number');
             primaryScreen.textContent = a;
+            secondaryScreen.textContent = '';
         } else {
             a += this.getAttribute('data-number');
             primaryScreen.textContent = a;
         }
     } else if (op) {
-        if (!b) {
+        if (b === null) {
             b = this.getAttribute('data-number');
             primaryScreen.textContent = b;
         } else {
@@ -44,11 +45,11 @@ function getNumber() {
 }
 
 function setOperator(e) {
-     if (op && b) {
+     if (op && (b)) { 
         doFunc(e);
         a = result;
         result = null;
-    } else if (result) {
+    } else if (result) { // the equality test returns false if `result` will be equal to 0 (number type not string). For example, if the return to the calculate method is not transformed to a string, and is equal to 0 number type. Alternative: `if (result !== null)`, will work with `result = 0`. Same logic applies to `a` and `b`. Both methods work and it depends on the type of behavior is wanted for the calculator to have.
         a = result;
         result = null;
     } 
@@ -56,6 +57,7 @@ function setOperator(e) {
     if (a) {
         op = this.getAttribute('data-operator');
         secondaryScreen.textContent = a + " " + op + " ";
+        primaryScreen.textContent = b;
     }
     
     console.log('--------setOperator----------')
@@ -69,7 +71,7 @@ function doFunc(e) {
     
     const funcButton = e.target.getAttribute('data-func');
     const isOpPressedTwice = e.target.hasAttribute('data-operator');
-    if ((funcButton == '=' || isOpPressedTwice) && b) {
+    if ((funcButton == '=' || isOpPressedTwice) && (b !== null)) {
         result = calc.calculate(Number(a), Number(b), op).toString();
         primaryScreen.textContent = result;
         secondaryScreen.textContent += b + ' =';
@@ -83,33 +85,34 @@ function doFunc(e) {
         b = null;
         op = null;
         result = null;
-        primaryScreen.textContent = '0';
+        primaryScreen.textContent = '';
         secondaryScreen.textContent = '';
     }
 
     if (funcButton == 'del') {
-        if (!op && !result) {
+        if (a && !op) {
             a = a.split('').slice(0, a.length - 1).join('');
             primaryScreen.textContent = a;
             if (a.length == 0) {
                 a = null;
-                primaryScreen.textContent = 0;
+                primaryScreen.textContent = '';
             }
         } else if (op && b) {
             b = b.split('').slice(0, b.length - 1).join('');
             primaryScreen.textContent = b;
             if (b.length == 0) {
                 b = null;
-                primaryScreen.textContent = 0;
+                primaryScreen.textContent = '';
             }
-        } else if (result) {
-            result = result.split('').slice(0, result.length - 1).join('');
-            primaryScreen.textContent = result;
-            if (result.length == 0) {
-                result = null;
-                primaryScreen.textContent = 0;
-            }
-        }
+        } 
+        //   else if (result) {
+        //     result = result.split('').slice(0, result.length - 1).join('');
+        //     primaryScreen.textContent = result;
+        //     if (result.length == 0) {
+        //         result = null;
+        //         primaryScreen.textContent = 0;
+        //     }
+        // }
     }
 
     console.log('--------doFunc----------')
@@ -124,21 +127,23 @@ function updateScreen(primaryScreenText, secondaryScreenText) {
     secondaryScreen = secondaryScreenText;
 }
 
+
+const numberBtns = document.querySelectorAll('button[data-number]');
+const operatorBtns = document.querySelectorAll('button[data-operator]');
+const funcBtns = document.querySelectorAll('button[data-func]');
 const primaryScreen = document.querySelector('.screen .primary');
 const secondaryScreen = document.querySelector('.screen .secondary');
 
-
-const numberBtns = document.querySelectorAll('button[data-number]');
 numberBtns.forEach(btn => {
     btn.addEventListener("click", getNumber);
     });
 
-const operatorBtns = document.querySelectorAll('button[data-operator]');
+
 operatorBtns.forEach(btn => {
     btn.addEventListener("click", setOperator);
     });
 
-const funcBtns = document.querySelectorAll('button[data-func]');
+
 funcBtns.forEach(btn => {
     btn.addEventListener("click", doFunc);
     });
